78744152945e91691ac7d4bc02d4c2245d7f8e97
jcesar <jcesar020@gmail.com>
2023-03-19 23:18:55 -0600

    Creando modelo estado de educacion, con sus controladorese, factorias

---
+++ app/Http/Controllers/EstadoEducacionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EstadoEducacion;
+use Illuminate\Http\Request;
+
+class EstadoEducacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EstadoEducacion  $estadoEducacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EstadoEducacion $estadoEducacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EstadoEducacion  $estadoEducacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EstadoEducacion $estadoEducacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EstadoEducacion  $estadoEducacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EstadoEducacion $estadoEducacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EstadoEducacion  $estadoEducacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EstadoEducacion $estadoEducacion)
+    {
+        //
+    }
+}
